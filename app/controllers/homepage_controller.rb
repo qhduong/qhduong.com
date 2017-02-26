65d00aa17f01e7ed9cb4b0732a36baffc52765d7
Hieu Duong <hieu@qhduong.com>
2017-02-25 16:39:00 -0800

    Update ruby version to 2.2.4. Update app to use sinatra. Only display hello world.

--- app/controllers/homepage_controller.rb
+++
@@ -1,6 +0,0 @@
-class HomepageController < ActionController::Base
-  protect_from_forgery
-
-  def index
-  end
-end
--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Qhduong::Application.config.session_store :encrypted_cookie_store, key: '_qhduong_session'
