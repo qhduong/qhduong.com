55feb3a3cb57f15dc4b7df0a4610b7328b3a5d1d
Hieu Duong <hieu@qhduong.com>
2017-02-26 00:39:28 -0800

    Add assets

---
+++ src/components/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { Footer } from "./Footer";
+
+ReactDOM.render(
+    <Footer />,
+    document.getElementById("root")
+);
